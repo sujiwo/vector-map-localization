17374ab1a122e04188e1120451e855f21f577d45
Adi Sujiwo <sujiwo@gmail.com>
2015-08-24 18:07:40 +0900

    Fix

--- vectormap_slam/projectpoint.py
+++ vectormap_slam/projectpoint.py
@@ -7,7 +7,10 @@ import math
 points = np.array([[0, 0, -2],
     [1, 0, -2],
     [1, 1, -2],
-    [0, 1, -2]])
+    [0, 1, -2]], dtype=np.float)
+
+width = 800
+height = 600
 
 
 def degreeToRadian (degree) :
@@ -30,7 +33,7 @@ def lookAt (eyePos, centerOfView, upVect):
     
     return viewMat
 
-def perspective (fovy, aspectRatio, zNear, zFar):
+def perspective1 (fovy, aspectRatio, zNear, zFar):
 #     projectionMatrix = Matrix4::Zero();
 #     double angle = degreeToRadian(fovy/2);
 #     double f = 1 / (tan(angle));
@@ -47,7 +50,25 @@ def perspective (fovy, aspectRatio, zNear, zFar):
     projMat [2, 2] = (zFar + zNear) / (zNear - zFar)
     projMat [3, 2] = -1
     projMat [2, 3] = 2 * zFar * zNear / (zNear - zFar)
-    return projMat  
+    return projMat
+
+# def perspective2 (f, ):
+
+
+def plotPoints (image, pointList):
+    origX = image.shape[1] / 2
+    origY = image.shape[0] / 2
+    for point in pointList:
+        point = point[0]
+        u = origY * (1 + point[0])
+        v = origX * (1 + point[1])
+        u = int(u)
+        v = int(v)
+        image [u, v] = 255
+        image [u+1, v+1] = 255
+        image [u, v+1] = 255
+        image [u+1, v] = 255
+    pass  
 
 
 
@@ -55,10 +76,17 @@ if __name__ == '__main__' :
     viewMat = lookAt (np.array([-0.5, -0.5, 2]),
         np.array([0.5, 0.5, -2]),
         np.array([0, 1, 0]))
-    projMat = perspective(45.0, 1.33, 1.0, 100)
+#     projMat = perspective1 (45.0, 1.33, 1.0, 100)
+    projMat = np.eye(3)
     
-    image = np.zeros ((1024,748,3), dtype=np.uint8)
-    rv, jac = cv2.Rodrigues(viewMat[0:3, 0:3])
-    imgpts, jac = cv2.projectPoints(points, rv, viewMat[0:3, 3], projMat, 0)
+    image = np.zeros ((height,width), dtype=np.uint8)
+    rv, jac1 = cv2.Rodrigues(viewMat[0:3, 0:3])
+#     imgpts, jac2 = cv2.projectPoints(points, rv, viewMat[0:3, 3], projMat[0:3,0:3], 0)
+    imgpts, jac2 = cv2.projectPoints (points, (0,0,0), (0,0,0), np.eye(3), 0)
+    plotPoints (image, imgpts)
+    
+    cv2.startWindowThread()
+    cv2.namedWindow("test")
+    cv2.imshow("test", image)
     
     pass
